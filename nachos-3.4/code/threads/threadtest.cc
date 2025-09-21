6b4c66c8e207cfe5a3c9679d21f39e7f566dccab
Deijenqgit config --global user.name Deijen <dseve008@fiu.edu>
2025-09-21 10:32:58 -0400

    DeijenExercise1Part3

--- nachos-3.4/code/threads/threadtest.cc
+++ nachos-3.4/code/threads/threadtest.cc
@@ -13,6 +13,13 @@
 #include "system.h"
 int SharedVariable = 0;
 
+#ifdef HW1_SEMAPHORES
+Semaphore *sharedVarSemaphore = NULL;
+Semaphore *barrierSemaphore = NULL;
+int totalThreads =0;
+int threadsCompleted = 0;
+#endif
+
 // testnum is set in main.cc
 int testnum = 1;
 
@@ -27,14 +34,48 @@ int testnum = 1;
 
 void SimpleThread(int which) {
 int num, val;
-for(num = 0; num < 5; num++) {
-val = SharedVariable;
-printf("*** thread %d sees value %d\n", which, val);
-SharedVariable = val+1;
-currentThread->Yield();
-}
-val = SharedVariable;
-printf("Thread %d sees final value %d\n", which, val);
+
+#ifdef HW1_SEMAPHORES
+    if (which == 0 && sharedVarSemaphore == NULL){
+        sharedVarSemaphore = new Semaphore("SharedVarSemaphore", 1);
+        barrierSemaphore = new Semaphore("BarrierSemaphore", 0);
+        totalThreads = testnum + 1;  // +1 for the main thread (thread 0)
+    }
+#endif
+
+    for(num = 0; num < 5; num++) {
+        val = SharedVariable;
+
+#ifdef HW1_SEMAPHORES
+        sharedVarSemaphore->P();
+#endif
+
+        printf("*** thread %d sees value %d\n", which, val);
+        SharedVariable = val+1;
+
+#ifdef HW1_SEMAPHORES
+        sharedVarSemaphore->V();
+#endif
+        currentThread->Yield();
+    }
+
+    val = SharedVariable;
+    printf("Thread %d sees final value %d\n", which, val);
+
+#ifdef HW1_SEMAPHORES
+    sharedVarSemaphore->P();
+    threadsCompleted++;
+
+    if(threadsCompleted == totalThreads) {
+        for (int i = 0; i < totalThreads - 1; i++){
+        barrierSemaphore-V();
+    }
+    threadsCompleted = 0;
+    } else {
+        sharedVarSemaphore->V();
+        barrierSemaphore-P();
+    }
+#endif
 }
 
 
@@ -63,6 +104,21 @@ ThreadTest1()
 void
 ThreadTest(int n)
 {
+#ifdef HW1_SEMAPHORES
+    sharedVairable = 0;
+    threadsCompleted =0;
+    totalThreads = n + 1;
+
+    if(sharedVarSemaphore !=NULL){
+        delete sharedVarSemaphore;
+        sharedVarSemaphore = NULL;
+    }
+    if (barrierSemaphore != NULL){
+        delete barrierSemaphore;
+        barrierSemaphore = NULL;
+    }
+#endif
+
     for (int i = 1; i <= n; i++) {
         Thread *t = new Thread("forked thread");
         t->Fork(SimpleThread, i);
